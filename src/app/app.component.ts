0a417ecf1d88b2181b6eff40c402aac76e4c752d
Evgen <baltatvik3@mail.ru>
2024-11-16 17:44:40 +0300

    release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BoardModule } from './board/board.module'; // Импортируйте модуль
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoardModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'my-app';
 }
